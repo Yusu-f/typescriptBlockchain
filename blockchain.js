8f18c5a2a8bdf96717a262c8d455b7dd8aa84e26
Yusu-f <yusouphishola01@gmail.com>
2022-05-12 19:28:41 +0100

    Final commit

--- blockchain.js
+++ blockchain.js
@@ -119,4 +119,4 @@ const alice = new Wallet();
 satoshi.sendMoney(50, bob.publicKey);
 bob.sendMoney(23, alice.publicKey);
 alice.sendMoney(5, bob.publicKey);
-console.log(Chain.instance);
+console.log(Chain.instance); //changes
